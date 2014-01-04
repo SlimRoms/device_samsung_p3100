7b96d54ce34fe053c3654d4251f59f62398dbfdb
Vladislav Koldobskiy <admin@nevergone.ru>
2014-01-04 14:16:48 -0500

    CommonMK: Update drawable settings
    Change-Id: Ic78372a2e3f0090294893c843b572e93d645eb84

--- p31xx-common.mk
+++ p31xx-common.mk
@@ -27,9 +27,8 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 LOCAL_PATH := device/samsung/p3100
 
 # Enable higher-res drawables while keeping mdpi as primary source
-PRODUCT_AAPT_CONFIG := large mdpi hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := mdpi
-PRODUCT_LOCALES += mdpi
+PRODUCT_AAPT_CONFIG := normal large tvdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
 # Init files
 PRODUCT_COPY_FILES += \
