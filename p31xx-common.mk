ed1bc6f4ec465e768c1ff93b1c3454c9d703158c
Andreas Blaesius <skate4life@gmx.de>
2015-07-16 21:53:04 +0200

    P31XX: cleanup / rework ramdisk parts for better readability (1/2)
    twrp.fstab and init.espresso.rc need to be placed inside
    /device/samsung/p3100/recovery/root because they else don't get included
    to the recovery ramdisk.
    
    Change-Id: Ic5ea906ccbc348a4201628ac3269f5500e7830f7

--- p31xx-common.mk
+++ p31xx-common.mk
@@ -32,14 +32,17 @@ PRODUCT_AAPT_CONFIG := large
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 PRODUCT_LOCALES += mdpi
 
-# Init files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.espresso.usb.rc:root/init.espresso.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.espresso.rc:root/init.espresso.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.espresso.rc:root/ueventd.espresso.rc \
-    $(LOCAL_PATH)/rootdir/fstab.espresso:root/fstab.espresso \
-    $(LOCAL_PATH)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
-    $(LOCAL_PATH)/rootdir/init.recovery.espresso.rc:/root/init.recovery.espresso.rc
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.espresso \
+    init.espresso.usb.rc \
+    init.espresso.rc \
+    ueventd.espresso.rc
+
+# Recovery Ramdisk
+PRODUCT_PACKAGES += \
+    init.recovery.espresso.rc \
+    twrp.fstab
 
 # GPS
 PRODUCT_COPY_FILES += \
