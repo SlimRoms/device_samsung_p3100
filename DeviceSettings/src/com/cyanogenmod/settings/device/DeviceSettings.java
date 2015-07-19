320b07ca53440c294699dd97fe0206da98116175
Andreas Blaesius <skate4life@gmx.de>
2015-07-19 13:49:23 +0200

    DeviceSettings: Checkboxes to switches
    Change-Id: Idb404cafd8aa632efcb243086a5ca1ca08ae6a60

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2012 The CyanogenMod Project
- *
+ * Copyright (C) 2015 SlimRoms
+
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
