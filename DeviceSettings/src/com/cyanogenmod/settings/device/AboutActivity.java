1947cc9b24cecabd38c2c6e5d14f0653abf16cb8
Andreas Blaesius <skate4life@gmx.de>
2015-07-19 16:51:11 +0200

    (WIP) DeviceSettings: Add "About Tab"
    Change-Id: Icb31303d650eb9531d4d12dfd4089c68c053c261

---
+++ DeviceSettings/src/com/cyanogenmod/settings/device/AboutActivity.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015 SlimRoms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button thanksSumm = (Button) findViewById(R.id.about_thanks_summary);
+        thanksSumm.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent("android.intent.action.VIEW",
+                        Uri.parse("https://crowdin.net/project/slimroms")));
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.device_settings, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu (Menu menu) {
+        menu.removeItem(R.id.action_about);
+        return true;
+    }
+
+}
