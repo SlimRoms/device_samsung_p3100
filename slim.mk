d25b84c7efcb2e80b37026e7e149714d25d24df8
andi34 <skate4life@gmx.de>
2015-08-12 12:41:03 +0200

    P31xx: move to espresso-common repo (1/2)
    Change-Id: If95daa33ddb81c48920b2914b7e97863c2c45881

--- slim.mk
+++ slim.mk
@@ -34,7 +34,7 @@ $(call inherit-product, device/samsung/p3100/full_p3100.mk)
 
 # SlimRoms specific overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/p3100/overlay/slim
-DEVICE_PACKAGE_OVERLAYS += device/samsung/p3100/overlay/slim-common
+DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso-common/overlay/slim-common
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p3100
