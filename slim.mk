e5e41c0ab2227dd86f54c8b1ee36dbe9a0a86839
Ziyan <jaraidaniel@gmail.com>
2016-01-03 23:30:03 +0100

    P3100: device tree cleanup
    Only cosmetical changes
    - fix some typos
    - use aosp instead full makefile
    - remove unused CleanSpec.mk
    
    Change-Id: I9f85b1483e3abc0431980a657858648a438d6073

--- slim.mk
+++ slim.mk
@@ -15,12 +15,6 @@
 # limitations under the License.
 #
 
-# use 44.1 kHz sounds
-TARGET_NEED_44KHZ_SOUNDS := true
-
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := p3100
 
@@ -29,26 +23,21 @@ PRODUCT_RELEASE_NAME := p3100
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 600
 
-# Inherit some common Slim stuff.
+# Inherit common Slim configuration
+$(call inherit-product, vendor/slim/config/gsm.mk)
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/p3100/full_p3100.mk)
 
 # SlimRoms specific overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/p3100/overlay/slim
 DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso-common/overlay/slim-common
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := p3100
-PRODUCT_NAME := slim_p3100
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-P3100
-PRODUCT_MANUFACTURER := samsung
+# Inherit device configuration
+$(call inherit-product, device/samsung/p3100/aosp_p3100.mk)
 
-#Set build fingerprint / ID / Prduct Name ect.
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := slim_p3100
+
+# Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=espressorfxx \
-    TARGET_DEVICE=espressorf \
     BUILD_FINGERPRINT="samsung/espressorfxx/espressorf:4.2.2/JDQ39/P3100XWDNA1:user/release-keys" \
     PRIVATE_BUILD_DESC="espressorfxx-user 4.2.2 JDQ39 P3100XWDNA1 release-keys"
